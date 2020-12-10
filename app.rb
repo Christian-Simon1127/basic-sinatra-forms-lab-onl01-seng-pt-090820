5fe33a1b5d1e3e25a006dd01f54b4b65c9f45bb4
Christian Simon <csimon1127@gmail.com>
2020-12-10 21:54:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do 
-    
+    erb :newteam
   end
 
   post '/team' do 
-    @team = 
+    @team = params
     erb :team
   end
 
